496af61529c86e1bfffa9c1b2b945560fba265f9
jon-zawada <jonathan_zawada@yahoo.com>
2025-05-25 14:16:46 -0700

    feat: electron-builder with scripts for mac win linux

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -4,4 +4,4 @@ import path from "path";
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({});
   mainWindow.loadFile(path.join(app.getAppPath(), "dist-react/index.html")); //fix this path later
-})
+});
